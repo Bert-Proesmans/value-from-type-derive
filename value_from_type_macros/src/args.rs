faaf548729ad02e78bbc80d80e3bd1f881c6f8c8
Bert Proesmans <bproesmans@hotmail.com>
2018-04-03 21:16:11 +0200

    Overhauled code
     - More modular code
     - Easier to extend
     - Fixed macro hygiene issues

---
+++ value_from_type_macros/src/args.rs
@@ -0,0 +1,26 @@
+use syn::Ident;
+use syn::punctuated::Punctuated;
+use syn::synom::Synom;
+
+/// Structure used as knowledge base for macro arguments.
+pub struct AttrArgs {
+    args: Vec<Ident>,
+}
+
+impl Synom for AttrArgs {
+    named!(parse -> Self, map!(
+		parens!(Punctuated::<Ident, Token![,]>::parse_terminated_nonempty),
+		|(_parens, args)| AttrArgs {
+			args: args.into_iter().collect(),
+		}
+	));
+}
+
+impl AttrArgs {
+    /// Returns the name chosen by the user.
+    pub fn enum_name(&self) -> Ident {
+        assert!(self.args.len() > 0);
+        // Ident implements copy
+        self.args[0]
+    }
+}
